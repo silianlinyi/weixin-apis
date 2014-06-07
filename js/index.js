54136f3e9114b7a42972ee9540d3c7d37983b380
silianlinyi <244098979@qq.com>
2014-06-07 10:06:29 +0800

    update document

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+$(function() {
 
+	$('#sidebar .header.item').on("click", function() {
+		$('#sidebar a.item').css('display', 'none');
+		$(this).siblings().css('display', 'block');
+	});
+
+	
+
+});
